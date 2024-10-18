867a027474f8b73ee8a6d68ec3dae04adebaaa1d
nalek0 <belash.aleksandr777@gmail.com>
2024-10-18 09:34:48 +0300

    Visually pressing the buttons

--- Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/rm/ResourceManager.kt
+++ Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/rm/ResourceManager.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -26,38 +26,78 @@ class ResourceManager {
 
     @Composable
     private fun menu() {
-
         Box(modifier = Modifier.background(color = Color(0xFF3A3A3A)).fillMaxHeight().width(MENU_WIDTH)) {
             Column(modifier = Modifier.padding(5.dp)) {
+                var sourcesEnabled by remember { mutableStateOf(false) }
+                var effectsEnabled by remember { mutableStateOf(true) }
+                var presetsEnabled by remember { mutableStateOf(true) }
+                var templatesEnabled by remember { mutableStateOf(true) }
+                val colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color(0xFF3A3A3A),
+                    contentColor = Color.White,
+                    disabledBackgroundColor = Color(0xFF222222),
+                    disabledContentColor = Color.White,
+                )
+
                 Button(
-                    onClick = ::menuButtonClick,
+                    onClick = {
+                        sourcesEnabled = false
+                        effectsEnabled = true
+                        presetsEnabled = true
+                        templatesEnabled = true
+                    },
                     modifier = Modifier.fillMaxWidth().height(MENU_BUTTON_HEIGHT),
-                    border = null,
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color(0xFF3A3A3A),
-                        contentColor = Color.White,
-                        disabledBackgroundColor = Color(0xFF222222),
-                        disabledContentColor = Color.White,
-                    ),
+                    colors = colors,
                     elevation = null,
-                    enabled = false
+                    border = null,
+                    enabled = sourcesEnabled
                 ) {
                     Text("Sources")
                 }
                 Button(
-                    onClick = ::menuButtonClick,
+                    onClick = {
+                        sourcesEnabled = true
+                        effectsEnabled = false
+                        presetsEnabled = true
+                        templatesEnabled = true
+                    },
                     modifier = Modifier.fillMaxWidth().height(MENU_BUTTON_HEIGHT),
-                    border = null,
-                    colors = ButtonDefaults.buttonColors(
-                        backgroundColor = Color(0xFF3A3A3A),
-                        contentColor = Color.White,
-                        disabledBackgroundColor = Color(0xFF222222),
-                        disabledContentColor = Color.White,
-                    ),
+                    colors = colors,
                     elevation = null,
-                    enabled = true,
+                    border = null,
+                    enabled = effectsEnabled
                 ) {
                     Text("Effects")
+                }
+                Button(
+                    onClick = {
+                        sourcesEnabled = true
+                        effectsEnabled = true
+                        presetsEnabled = false
+                        templatesEnabled = true
+                    },
+                    modifier = Modifier.fillMaxWidth().height(MENU_BUTTON_HEIGHT),
+                    colors = colors,
+                    elevation = null,
+                    border = null,
+                    enabled = presetsEnabled
+                ) {
+                    Text("Presets")
+                }
+                Button(
+                    onClick = {
+                        sourcesEnabled = true
+                        effectsEnabled = true
+                        presetsEnabled = true
+                        templatesEnabled = false
+                    },
+                    modifier = Modifier.fillMaxWidth().height(MENU_BUTTON_HEIGHT),
+                    colors = colors,
+                    elevation = null,
+                    border = null,
+                    enabled = templatesEnabled
+                ) {
+                    Text("Templates")
                 }
             }
         }
