d5e604f018c0d4b64b273226486787894b963fb1
nalek0 <belash.aleksandr777@gmail.com>
2024-10-18 16:07:08 +0300

    State usage to change active button

--- Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/rm/ResourceManager.kt
+++ Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/rm/ResourceManager.kt
@@ -2,12 +2,23 @@ package org.legalteamwork.silverscreen.rm
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 class ResourceManager {
+
+    private val buttonIndex = mutableStateOf(0)
+    private val buttonList = listOf(
+        MenuButton("Sources"),
+        MenuButton("Effects"),
+        MenuButton("Presets"),
+        MenuButton("Templates"),
+    )
 
     @Composable
     fun resourceManager() {
@@ -17,18 +28,46 @@ class ResourceManager {
         }
     }
 
-    private fun menuButtonClick() {
-        // TODO
+    @Composable
+    private fun menu() {
+        Box(modifier = Modifier.background(color = Color(0xFF3A3A3A)).fillMaxHeight().width(MENU_WIDTH)) {
+            buttonList()
+        }
     }
 
     @Composable
-    private fun menu() {
-        val menu = Menu()
+    fun buttonList() {
+        Column(modifier = Modifier.padding(5.dp)) {
+            for (buttonWithIndex in buttonList.withIndex()) {
+                val value = buttonWithIndex.value
+                val index = buttonWithIndex.index
 
-        Box(modifier = Modifier.background(color = Color(0xFF3A3A3A)).fillMaxHeight().width(MENU_WIDTH)) {
-            Column(modifier = Modifier.padding(5.dp)) {
-                menu.buttonList()
+                menuButton(value.title, index)
             }
+        }
+    }
+
+    @Composable
+    private fun menuButton(title: String, index: Int) {
+        var chosenButton by remember { buttonIndex }
+        val buttonColors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFF3A3A3A),
+            contentColor = Color.White,
+            disabledBackgroundColor = Color(0xFF222222),
+            disabledContentColor = Color.White,
+        )
+
+        Button(
+            onClick = {
+                chosenButton = index
+            },
+            modifier = Modifier.fillMaxWidth().height(MENU_BUTTON_HEIGHT),
+            colors = buttonColors,
+            elevation = null,
+            border = null,
+            enabled = chosenButton != index
+        ) {
+            Text(title)
         }
     }
 
