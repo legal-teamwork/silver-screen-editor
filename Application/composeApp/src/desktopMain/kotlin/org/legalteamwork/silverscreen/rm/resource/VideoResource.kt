49fd98f222bdb9f459acd59bea3058fc499641fe
nalek0 <belash.aleksandr777@gmail.com>
2024-10-23 10:54:43 +0300

    ffmpeg usage instead

--- Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/rm/resource/VideoResource.kt
+++ Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/rm/resource/VideoResource.kt
@@ -1,10 +1,10 @@
 package org.legalteamwork.silverscreen.rm.resource
 
-import org.bytedeco.opencv.global.opencv_videoio.CAP_PROP_FRAME_COUNT
-import org.bytedeco.opencv.opencv_videoio.VideoCapture
+import org.bytedeco.javacv.FFmpegFrameGrabber
 
-class VideoResource(override val title: String, val resourcePath: String) : Resource {
+class VideoResource(_title: String, val resourcePath: String) : Resource {
 
+    override val title: String = "$_title : $numberOfFrames"
 
     /**
      * Path to the resource preview, currently - path in the project resource folder
@@ -19,20 +19,20 @@ class VideoResource(override val title: String, val resourcePath: String) : Reso
 
     /**
      * Number of frames in the video resource
-     *
-     * @see CAP_PROP_FRAME_COUNT
      */
     val numberOfFrames: Int
         get() {
-            val videoCapture = VideoCapture()
+            val frameGrabber = FFmpegFrameGrabber(resourcePath)
+            
+            try {
+                frameGrabber.start()
+                val result = frameGrabber.lengthInFrames
+                frameGrabber.stop()
+                frameGrabber.close()
 
-            if (!videoCapture.open(resourcePath)) {
-                // Not opened, something happened
-                // TODO: make pop up window with error description
-
-                throw RuntimeException("Cannot open resource $resourcePath")
-            } else {
-                return videoCapture.get(CAP_PROP_FRAME_COUNT).toInt()
+                return result
+            } catch (e: FFmpegFrameGrabber.Exception) {
+                return -1
             }
         }
 
