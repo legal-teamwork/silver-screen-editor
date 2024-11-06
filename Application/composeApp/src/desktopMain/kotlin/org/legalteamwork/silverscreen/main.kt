2b9bc00947b6db6d5afc826d4328c358c044d50c
reas <reas.qf0@gmail.com>
2024-11-06 14:33:10 +0300

    set minimum window size

--- Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/main.kt
+++ Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/main.kt
@@ -7,6 +7,8 @@ import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import org.legalteamwork.silverscreen.rm.VideoEditor
 import org.legalteamwork.silverscreen.rm.resource.VideoResource
+import java.awt.Dimension
+import java.awt.Toolkit
 
 fun main() {
     //Test
@@ -36,6 +38,8 @@ fun main() {
             title = "Silver Screen Editor",
             icon = icon
         ) {
+            val screenSize = Toolkit.getDefaultToolkit().screenSize
+            window.minimumSize = Dimension(screenSize.width / 2, screenSize.height / 2)
             App()
         }
     }
