82de80c89e5167c0ae8f1b96758ee11e1c3c6d9d
nalek0 <belash.aleksandr777@gmail.com>
2024-12-03 11:43:32 +0300

    Removed unused code

--- Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/windows/WindowManager.kt
+++
@@ -1,9 +0,0 @@
-package org.legalteamwork.silverscreen.windows
-
-import androidx.compose.runtime.MutableState
-import androidx.compose.ui.unit.Dp
-
-class WindowManager(
-    val windowBlock: WindowBlock
-) {
-}
