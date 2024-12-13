864838204466214e79f445415d71bbcc402cc619
Aleksandr Belash <belash.aleksandr777@gmail.com>
2024-12-14 00:47:58 +0300

    Removed unused resource manager tabs

---
+++ Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/rm/window/effects/EffectsMainWindow.kt
@@ -0,0 +1,9 @@
+package org.legalteamwork.silverscreen.rm.window.effects
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EffectsMainWindow() {
+    Text("EFFECTS_WINDOW")
+}
