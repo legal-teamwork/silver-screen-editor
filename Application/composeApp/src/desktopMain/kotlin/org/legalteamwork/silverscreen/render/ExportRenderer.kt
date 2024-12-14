e94037fc1de08784cb5ed330feac8c6309f10835
reas <reas.qf0@gmail.com>
2024-12-14 16:19:42 +0300

    speed up rendering (by a lot)

--- Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/render/ExportRenderer.kt
+++ Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/render/ExportRenderer.kt
@@ -4,46 +4,48 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.bytedeco.ffmpeg.global.avcodec
 import org.bytedeco.ffmpeg.global.avutil
 import org.bytedeco.javacv.*
+import org.bytedeco.opencv.global.opencv_core.*
+import org.bytedeco.opencv.opencv_core.Mat
+import org.bytedeco.opencv.opencv_core.Size
+import org.bytedeco.opencv.global.opencv_imgproc.*
+import org.bytedeco.opencv.opencv_core.Scalar
 import org.legalteamwork.silverscreen.re.VideoEditor
 import org.legalteamwork.silverscreen.save.Project
 import org.legalteamwork.silverscreen.save.Resolution
-import java.awt.Graphics2D
-import java.awt.Image
-import java.awt.image.BufferedImage
-import java.nio.ByteBuffer
 import kotlin.math.max
+import kotlin.system.measureTimeMillis
 
 private val logger = KotlinLogging.logger {}
 
-private fun resizeImage(originalImage: BufferedImage, targetWidth: Int, targetHeight: Int): BufferedImage {
-    // Calculate the aspect ratio
-    val originalWidth = originalImage.width
-    val originalHeight = originalImage.height
+private fun resizeImage(originalImage: Mat, targetWidth: Int, targetHeight: Int): Mat {
+    val originalWidth = originalImage.cols()
+    val originalHeight = originalImage.rows()
     if (targetWidth == originalWidth && targetHeight == originalHeight)
         return originalImage
 
-    val aspectRatio = originalWidth.toDouble() / originalHeight.toDouble()
-
-    // Determine new dimensions while preserving aspect ratio
     var newWidth = targetWidth
     var newHeight = targetHeight
 
-    if (originalWidth > originalHeight) {
-        newHeight = (targetWidth / aspectRatio).toInt()
+    if (originalWidth * targetHeight > originalHeight * targetWidth) {
+        newHeight = originalHeight * targetWidth / originalWidth
     } else {
-        newWidth = (targetHeight * aspectRatio).toInt()
+        newWidth = originalWidth * targetHeight / originalHeight
     }
 
-    // Create a new BufferedImage with the new dimensions
-    val resizedImage = BufferedImage(targetWidth, targetHeight, originalImage.type)
+    val resizedImage = Mat()
+    resize(originalImage, resizedImage, Size(newWidth, newHeight))
 
-    // Draw the original image into the resized image
-    val g2d: Graphics2D = resizedImage.createGraphics()
-    g2d.drawImage(originalImage.getScaledInstance(newWidth, newHeight, Image.SCALE_FAST),
-        (targetWidth - newWidth) / 2, (targetHeight - newHeight) / 2, null)
-    g2d.dispose()
+    if (newWidth == targetWidth && newHeight == targetHeight)
+        return resizedImage
 
-    return resizedImage
+    println("$originalWidth $originalHeight $targetWidth $targetHeight $newWidth $newHeight")
+    val borderedImage = Mat()
+    copyMakeBorder(resizedImage, borderedImage,
+        (targetHeight - newHeight) / 2, (targetHeight - newHeight) / 2,
+        (targetWidth - newWidth) / 2, (targetWidth - newWidth) / 2,
+        BORDER_CONSTANT, Scalar(0.0, 0.0, 0.0, 0.0)
+    )
+    return borderedImage
 }
 
 /*
@@ -63,84 +65,59 @@ class ExportRenderer {
         val resources = VideoEditor.VideoTrack.resourcesOnTrack.sortedBy { it.position }
         logger.info { "export start; expected total frames: ${resources.lastOrNull()?.run { position + framesCount } ?: 0}" }
 
-        val recorder = FFmpegFrameRecorder(filename, width, height)
-        recorder.format = "mp4"
-        recorder.frameRate = fps
-        recorder.videoBitrate = Project.get { bitrate * 1000 }
-        recorder.videoCodec = avcodec.AV_CODEC_ID_H264
-        recorder.pixelFormat = avutil.AV_PIX_FMT_YUV420P
-        VideoEditor.VideoTrack.videoResources.firstOrNull()?.let {
-            val grabber = FFmpegFrameGrabber(it.resourcePath)
-            grabber.start()
-            recorder.audioCodec = grabber.audioCodec
-            recorder.audioBitrate = grabber.audioBitrate
-            recorder.audioChannels = grabber.audioChannels
-            recorder.sampleRate = grabber.sampleRate
-            grabber.stop()
-        }
-        recorder.start()
+        val exportTime = measureTimeMillis {
+            val recorder = FFmpegFrameRecorder(filename, width, height)
+            recorder.format = "mp4"
+            recorder.frameRate = fps
+            recorder.videoBitrate = Project.get { bitrate * 1000 }
+            recorder.videoCodec = avcodec.AV_CODEC_ID_H264
+            recorder.pixelFormat = avutil.AV_PIX_FMT_YUV420P
+            recorder.start()
 
-        fun next(grabber: FFmpegFrameGrabber): Frame {
-            while (true) {
-                val frame = grabber.grab()
-                //println(frame.type)
-                if (frame.type == Frame.Type.AUDIO)
-                    recorder.record(frame)
-                else
-                    return frame
-            }
-        }
+            val converter = OpenCVFrameConverter.ToMat()
+            val blankFrame = converter.convert(Mat(width, height, CV_8UC3, Scalar(0.0, 0.0, 0.0, 0.0)))
+            var lastFrame = 0
+            resources.forEach { resource ->
+                val blankFrames = max(0, resource.position - lastFrame)
 
-        val converter = Java2DFrameConverter()
-        var lastFrame = 0
-        resources.forEach { resource ->
-            val blankFrames = max(0, resource.position - lastFrame)
+                repeat(blankFrames) { recorder.record(blankFrame) }
+                lastFrame = resource.position
 
-            repeat(blankFrames) {
-                recorder.record(converter.convert(BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)))
-            }
-            /* TODO: audio should be handled differently
-            val length = (recorder.sampleRate * blankFrames / fps).toInt()
-            println("$blankFrames $length")
-            val bufferList = mutableListOf<ByteBuffer>()
-            repeat(recorder.audioChannels) {
-                val buffer = ByteBuffer.allocate(length)
-                bufferList.add(buffer)
-            }
-            recorder.recordSamples(*bufferList.toTypedArray())
-             */
-            lastFrame = resource.position
+                val videoResource = VideoEditor.getVideoResources()[resource.id]
+                val frameGrabber = FFmpegFrameGrabber(videoResource.resourcePath)
+                frameGrabber.start()
 
-            val videoResource = VideoEditor.getVideoResources()[resource.id]
-            val frameGrabber = FFmpegFrameGrabber(videoResource.resourcePath)
-            frameGrabber.start()
-
-            var cachedFrame: Frame? = null
-            var lastSourceFrame = -1
-            println(videoResource.numberOfFrames)
-            (resource.framesSkip..<resource.framesSkip + resource.framesCount).forEach { frameNo ->
-                val nextFrame = videoResource.fromProjectFPS(frameNo)
-                logger.info { "relative frame $frameNo; source frame $nextFrame" }
-                if (nextFrame > lastSourceFrame) {
-                    try {
-                        val frame = videoResource.getFrame(nextFrame)
-                        val image = frame.bufferedImage
-                        val resizedImage = resizeImage(image, width, height)
-                        val resizedFrame = converter.convert(resizedImage)
-                        cachedFrame = resizedFrame
-                        lastSourceFrame = nextFrame
-                        logger.info { "updated cached frame" }
-                    } catch (_: Exception) {}
+                var cachedFrame: Frame? = null
+                var lastSourceFrame = -1
+                //println(videoResource.numberOfFrames)
+                (resource.framesSkip..<resource.framesSkip + resource.framesCount).forEach { frameNo ->
+                    val nextFrame = videoResource.fromProjectFPS(frameNo)
+                    //logger.info { "relative frame $frameNo; source frame $nextFrame" }
+                    val start = System.currentTimeMillis()
+                    if (nextFrame > lastSourceFrame) {
+                        try {
+                            repeat(nextFrame - lastSourceFrame - 1) { frameGrabber.grabImage() }
+                            val frame = frameGrabber.grabImage()
+                            val image = converter.convert(frame)
+                            val resizedImage = resizeImage(image, width, height)
+                            val resizedFrame = converter.convert(resizedImage)
+                            cachedFrame = resizedFrame
+                            lastSourceFrame = nextFrame
+                            logger.info { "updated cached frame" }
+                        } catch (_: Exception) {}
+                    }
+                    recorder.record(cachedFrame)
+                    logger.info { "processing frame took ${System.currentTimeMillis() - start} ms" }
                 }
-                recorder.record(cachedFrame)
+                frameGrabber.stop()
+                frameGrabber.close()
+                logger.info { "export: $blankFrames padded frames, ${resource.framesCount} actual frames" }
+                lastFrame += resource.framesCount
             }
-            frameGrabber.stop()
-            frameGrabber.close()
-            logger.info { "export: $blankFrames padded frames, ${resource.framesCount} actual frames" }
-            lastFrame += resource.framesCount
+            recorder.stop()
+            recorder.close()
         }
-        logger.info { "export done" }
-        recorder.stop()
-        recorder.close()
+
+        logger.info { "export done; took $exportTime ms" }
     }
 }
