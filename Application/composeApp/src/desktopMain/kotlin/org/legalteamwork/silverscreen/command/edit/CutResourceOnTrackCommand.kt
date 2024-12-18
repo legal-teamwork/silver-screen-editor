6b6c960b87b0419aa7c40e4c0c14e1b94ee385bd
reas <reas.qf0@gmail.com>
2024-12-18 18:58:30 +0300

    fixing bugs

--- Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/command/edit/CutResourceOnTrackCommand.kt
+++ Application/composeApp/src/desktopMain/kotlin/org/legalteamwork/silverscreen/command/edit/CutResourceOnTrackCommand.kt
@@ -2,6 +2,7 @@ package org.legalteamwork.silverscreen.command.edit
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.legalteamwork.silverscreen.command.CommandUndoSupport
+import org.legalteamwork.silverscreen.re.DpInFrame
 import org.legalteamwork.silverscreen.re.ResourceOnTrack
 import org.legalteamwork.silverscreen.re.VideoTrack
 import org.legalteamwork.silverscreen.resources.Dimens
@@ -11,7 +12,7 @@ class CutResourceOnTrackCommand(
     private val position: Int,
     private val index: Int
 ) : CommandUndoSupport {
-    override val title: String = "Cut cmd"
+    override val title: String = "Cut command"
     override val description: String = "Cut track with the $position position"
     private val logger = KotlinLogging.logger {}
     private var leftResource: ResourceOnTrack? = null
@@ -20,8 +21,9 @@ class CutResourceOnTrackCommand(
     override fun execute() {
         logger.info { "Cutting video resource" }
 
-        val leftSize = position - VideoTrack.resourcesOnTrack[index].getLeftBorder()
-        val rightSize = VideoTrack.resourcesOnTrack[index].getRightBorder() - position + 1
+        val pos = (position / DpInFrame).toInt()
+        val leftSize = pos - VideoTrack.resourcesOnTrack[index].getLeftBorder()
+        val rightSize = VideoTrack.resourcesOnTrack[index].getRightBorder() - pos + 1
         if (leftSize >= Dimens.MIN_SIZE_OF_RESOURCE_ON_TRACK &&
             rightSize >= Dimens.MIN_SIZE_OF_RESOURCE_ON_TRACK
         ) {
@@ -29,7 +31,7 @@ class CutResourceOnTrackCommand(
             leftResource = track.resourcesOnTrack[index]
             rightResource = track.addResource(
                 track.videoResources[index],
-                position,
+                pos,
                 rightSize,
                 track.resourcesOnTrack[index].framesSkip + leftSize,
                 leftResource?.filters?.toList() ?: emptyList()
